7a623ddff4e6591da9a3627e7908efac0f76d2e8
Schmitt <git@schmida.net>
2021-07-02 21:27:04 +0200

    master: Added HealthController.

---
+++ backend/src/main/java/net/schmida/bookmark/backend/health/HealthController.java
@@ -0,0 +1,16 @@
+package net.schmida.bookmark.backend.health;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+
+    @GetMapping
+    public String health() {
+        return "ok";
+    }
+
+}
